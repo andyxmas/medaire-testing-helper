68ecc035573c427a225a49cf9f751b4761afe38f
andyxmas <andy@pixelcabin.io>
2022-12-19 13:31:57 +0000

    refactor structure and return

--- netlify/functions/metafield/index.js
+++ netlify/functions/metafield/index.js
@@ -6,8 +6,6 @@ const handler = async (event) => {
 
     if (!jsonKey) return { statusCode: 500, body: 'no JSON key provided. Pass e.g. ?json=nbq' }
     // really, we would check here for the existence of the json file
-    const json = require(`./json/${jsonKey}.json`)
-    const jsonString = JSON.stringify(json);
 
     const Shopify = require('shopify-api-node');
 
@@ -16,31 +14,35 @@ const handler = async (event) => {
       accessToken: process.env.ACCESS_TOKEN
     });
 
+
+    const json = require(`./json/${jsonKey}.json`)
+    const jsonString = JSON.stringify(json);
     const userId = 6577333993725
 
-    const metafields = await shopify.metafield
-      .list({
-        metafield: { owner_resource: 'customer', owner_id: userId }
-      })
+    const listMetafields = () => {
+      shopify.metafield
+        .list({
+          metafield: { owner_resource: 'customer', owner_id: userId }
+        })
+    }
 
-    shopify.metafield
-      .create({
-        key: 'data',
-        value: jsonString,
-        value_type: 'json_string',
-        namespace: 'quote',
-        owner_resource: 'customer',
-        owner_id: userId
-      })
-      .then(
-        (metafield) => console.log(metafield),
-        (err) => console.error(err)
-      );
-    console.log('YO')
+    const setMetafield = async () => {
+      return shopify.metafield
+        .create({
+          key: 'data',
+          value: jsonString,
+          value_type: 'json_string',
+          namespace: 'quote',
+          owner_resource: 'customer',
+          owner_id: userId
+        })
+    }
+
+    const updatedMetafield = await setMetafield();
 
     return {
       statusCode: 200,
-      body: JSON.stringify(metafields),
+      body: JSON.stringify(updatedMetafield),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
